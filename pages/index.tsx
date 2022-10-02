70b6c9b8bc47111c5c1c2ed56837af52e444998f
Vlad Somai <Vlad.Somai-3rdpty@brooks.com>
2022-10-02 14:56:26 +0300

    center the input box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/layout'
-import {
-  ReactElement,
-  SyntheticEvent,
-  useState,
-} from 'react'
+import { ReactElement, SyntheticEvent, useState } from 'react'
 import type { NextPageWithLayout } from './_app'
 import dynamic from 'next/dynamic'
 
 const Home: NextPageWithLayout = () => {
-  const [QrImage, setQrLink] = useState<string | null>(null)
   const [text, setText] = useState<string>('')
-  const size = 300
   const textChanged = (e: SyntheticEvent) => {
     const inputBox = e.target as HTMLInputElement
     setText(inputBox.value)
@@ -28,25 +21,14 @@ const Home: NextPageWithLayout = () => {
       </Head>
       <div className="flex justify-center">
         <div className="flex flex-col w-6/12 items-center bg-slate-200 rounded-box p-5 m-5">
-          <div className="flex flex-col w-3/6">
-            <h1 className="text-2xl mb-5">Generate a QR code</h1>
+          <div className="flex flex-col items-center m-5 w-full">
             <input
               onChange={textChanged}
-              className="input input-bordered max-w-xs mb-5"
+              className="input input-bordered w-3/6"
               placeholder="Enter some text to generate a QR code"
             />
           </div>
           <QRCodeComp data={text} />
-
-          {QrImage && (
-            <Image
-              src={QrImage as string}
-              width={size}
-              height={size}
-              alt="main picture"
-              priority
-            ></Image>
-          )}
         </div>
       </div>
     </>
